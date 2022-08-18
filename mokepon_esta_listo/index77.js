cb162355129fa81c1ff53e0e3324144c607465af
erickyb <richardyb1985@gmail.com>
2022-08-18 17:18:54 -0500

    datos personales actualizados 2022

--- mokepon_esta_listo/index77.js
+++ mokepon_esta_listo/index77.js
@@ -53,22 +53,6 @@ app.post("/mokepon/:jugadorId", (req, res) => {
   res.end()
 })
 
-/*app.post("/mokepon/:jugadorId/posicion", (req, res) => {
-
-  const jugadorId = req.params.jugadorId || ""
-  const x = req.body.x || 0
-  const y = req.body.y || 0 
-  const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
-
-  if (jugadorIndex >= 0) {
-    jugadores[jugadorIndex].  actualizarPosicion(x, y)
-  }
-   res.end()
-   
-  //const enemigos = jugadores.filter((jugador) => jugadorId !== jugador.id)
-  //res.send({ enemigos })
-
-})*/
 
 app.listen(8080, () => {
   console.log("Servidor funcionando")
